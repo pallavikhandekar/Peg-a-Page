7f6fa19e55f9571b05020cbdea03d1f761f7118e
Pallavi <pallavikhandekar212@gmail.com>
2013-11-12 07:44:30 -0800

    modified db settings

--- DjangoProject/settings.py
+++ DjangoProject/settings.py
@@ -14,10 +14,10 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'pegapage',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': '127.0.0.1',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '',                      # Set to empty string for default.
+        'USER': 'nimble',
+        'PASSWORD': 'password',
+        'HOST': 'ec2-50-19-213-178.compute-1.amazonaws.com',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'PORT': '3306',                      # Set to empty string for default.
     }
 }
 
