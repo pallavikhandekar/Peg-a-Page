de43f4a199fae06ebc7d7fbe86772b2a68039d23
sharanyaj <sharansmiley@gmail.com>
2013-11-13 11:51:07 -0800

    commit

--- DjangoProject/settings.py
+++ DjangoProject/settings.py
@@ -72,7 +72,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    "./DjangoProject/static",
+    "./Peg-a-Page/static",
 )
 
 # List of finder classes that know how to find static files in
@@ -110,7 +110,7 @@ WSGI_APPLICATION = 'DjangoProject.wsgi.application'
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    "./DjangoProject/Templates"
+    "./Peg-a-Page/Templates"
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
 )
