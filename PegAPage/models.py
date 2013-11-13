e9c6a294da4dc70b41e721a9e6619f54465d6fbd
sharanyaj <sharansmiley@gmail.com>
2013-11-13 11:24:42 -0800

    commit

--- PegAPage/models.py
+++ PegAPage/models.py
@@ -2,15 +2,19 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-class UrlLink(models.Model):
-    url = models.URLField(unique=True)
 
-class Bookmark(models.Model):
-    bookmarkname = models.CharField(max_length=200)
+class Board(models.Model):
+    Board_id = models.IntegerField(unique=True)
+    Board_name = models.CharField(max_length=200,unique=True)
     user = models.ForeignKey(User)
-    link = models.ForeignKey(UrlLink)
+    Board_des =  models.CharField(max_length=64)    
 
            
 class Peg(models.Model):
+    Peg_id = models.IntegerField(unique=True)
     name = models.CharField(max_length=64, unique=True)
-    bookmarks = models.ManyToManyField(Bookmark)
+    url = models.URLField(unique=True)
+    image = models.CharField(max_length=64)
+    peg_des =  models.CharField(max_length=64)
+    
+    boards = models.ManyToManyField(Board)
