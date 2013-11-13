0935fb75bfb40e5aeea28acaf6050ba436077dac
Pallavi <pallavikhandekar212@gmail.com>
2013-11-12 20:54:02 -0800

    Added edit image

--- DjangoProject/settings.py
+++ DjangoProject/settings.py
@@ -1,4 +1,5 @@
 # Django settings for DjangoProject project.
+import os
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -72,7 +73,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    "./DjangoProject/static",
+    "./Peg-a-Page/static",
 )
 
 # List of finder classes that know how to find static files in
@@ -110,7 +111,7 @@ WSGI_APPLICATION = 'DjangoProject.wsgi.application'
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    "./DjangoProject/Templates"
+    "./Peg-a-Page/Templates"
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
 )
