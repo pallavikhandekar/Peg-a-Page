e4be73b354420f7d72350d080293f710798c9fcc
kiranmohare <kiranmohare23@gmail.com>
2013-11-25 13:35:39 -0800

    Update views.py

--- PegAPage/views.py
+++ PegAPage/views.py
@@ -277,6 +277,8 @@ def LikePeg(request):
         if form.is_valid():
             print "VALID"
             # Like Peg
+            boardid = request.POST['boardid']
+            savePeg(form,boardid)
             #Like, dummy = Like.objects.get_or_create(
                 #Like_desc = request.POST['likedesc'],         
                 #user_id = request.POST['userid'], 
@@ -291,4 +293,8 @@ def LikePeg(request):
             form = LikePegForm(request.POST)
             variables = RequestContext(request, {'form': form})
             return render_to_response('Like_Peg.html', variables)
-    
+    else:
+        print "VALID Like"
+        form = LikePegForm()
+        variables = RequestContext(request, {'form': form})
+        return render_to_response('Like_Peg.html', variables)
